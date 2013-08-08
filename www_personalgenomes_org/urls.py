ec0d9f1d37cd4d508022c0029cd6c5b6a7b15ab9
Madeleine Ball <mpball@gmail.com>
2013-08-08 17:54:44 -0400

    Add global network, new PGP, and mission pages

--- www_personalgenomes_org/urls.py
+++ www_personalgenomes_org/urls.py
@@ -5,7 +5,8 @@ from django.views.generic import TemplateView
 # from django.contrib import admin
 # admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'www_personalgenomes_org.views.home', name='home'),
     # url(r'^www_personalgenomes_org/', include('www_personalgenomes_org.foo.urls')),
@@ -16,5 +17,14 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 
-    (r'^$', TemplateView.as_view(template_name='index.html')),
+    (r'^$', TemplateView.as_view(template_name='www_personalgenomes_org/index.html')),
+
+    # Global network pages
+    (r'^international.html', TemplateView.as_view(template_name='www_personalgenomes_org/network.html')),
+    (r'^network(.html|/|)$', TemplateView.as_view(template_name='www_personalgenomes_org/network.html')),
+    (r'^start-pgp(.html|/|)$', TemplateView.as_view(template_name='www_personalgenomes_org/start-pgp.html')),
+
+    # About pages
+    (r'^mission(.html|/|)$', TemplateView.as_view(template_name='www_personalgenomes_org/mission.html')),
+
 )
