0796179b2ddbf6462063f09c539582be80085c56
Madeleine Ball <mpball@gmail.com>
2013-08-08 13:51:24 -0400

    Set up first page for www.personalgenomes.org site

--- www_personalgenomes_org/settings.py
+++ www_personalgenomes_org/settings.py
@@ -2,6 +2,8 @@
 
 import os
 
+SITE_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -73,6 +75,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.normpath(os.path.join(SITE_ROOT, 'static')),
 )
 
 # List of finder classes that know how to find static files in
@@ -112,6 +115,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.normpath(os.path.join(SITE_ROOT, 'templates')),
 )
 
 INSTALLED_APPS = (
