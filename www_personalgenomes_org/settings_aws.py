1772072c2bca80d705a1b05b0a766391c896e358
Madeleine Ball <mpball@gmail.com>
2013-08-21 09:49:02 -0700

    Move settings to AWS-specific

--- www_personalgenomes_org/settings_aws.py
+++ www_personalgenomes_org/settings_aws.py
@@ -16,11 +16,11 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'www_personalgenomes_org',
-        'USER': 'pgorg',
-        'PASSWORD': 'glassworks',
-        'HOST': '',
-        'PORT': '',
+        'NAME': os.environ['RDS_DB_NAME'],
+        'USER': os.environ['RDS_USERNAME'],
+        'PASSWORD': os.environ['RDS_PASSWORD'],
+        'HOST': os.environ['RDS_HOSTNAME'],
+        'PORT': os.environ['RDS_PORT'],
     }
 }
 
