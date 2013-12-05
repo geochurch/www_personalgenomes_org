7300717e3ae43581f4bc9a9de2e9e51601deabea
Madeleine Ball <mpball@gmail.com>
2013-12-05 16:29:26 -0500

    Add content for PGP UK

--- uk/urls.py
+++ uk/urls.py
@@ -7,4 +7,8 @@ urlpatterns = patterns(
     '',
     url(r'^/?$', TemplateView.as_view(template_name='uk/index.html'),
         name='index'),
+    url(r'^/people/?', TemplateView.as_view(template_name='uk/people.html'),
+        name='people'),
+    url(r'^/enrollment/?', TemplateView.as_view(template_name='uk/enrollment.html'),
+        name='enrollment'),
     )
