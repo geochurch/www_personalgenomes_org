ba2b529df10ad27fbdeba27eba3f631c2c9b606b
Madeleine Ball <mpball@gmail.com>
2013-12-06 16:14:40 -0500

    Initial attempt at using GeoIP

--- www_personalgenomes_org/views.py
+++ www_personalgenomes_org/views.py
@@ -1,14 +1,23 @@
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse, reverse_lazy
 from django.views.generic import RedirectView
-from django.shortcuts import redirect
-
-class ReverseRedirectView(RedirectView):
-
-    def get_redirect_url(self):
-        return reverse_lazy(self.url, args=[u""])
-
+from django.shortcuts import redirect, render
+from django.contrib.gis.geoip import GeoIP
 
 def redirect_to_name(request, url_name):
     """Redirects to URL with name url_name"""
     return redirect(url_name)
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[-1].strip()
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+def index(request):
+    """Displays index with IP..."""
+    ip = get_client_ip(request)
+    g = GeoIP()
+    return render(request, 'www_personalgenomes_org/index.html', {'ip': ip, 'country': g.country(ip)})
