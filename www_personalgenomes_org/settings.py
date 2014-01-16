c4890410620f63c2e909da4160e4c16d5a1cd74e
Madeleine Ball <mpball@gmail.com>
2014-01-16 04:39:05 -0500

    Move translations to root directory

--- www_personalgenomes_org/settings.py
+++ www_personalgenomes_org/settings.py
@@ -2,8 +2,7 @@
 
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-SITE_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -72,7 +71,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.normpath(os.path.join(SITE_ROOT, 'static')),
+    os.path.normpath(os.path.join(BASE_DIR, 'static')),
 )
 
 # List of finder classes that know how to find static files in
@@ -113,7 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.normpath(os.path.join(SITE_ROOT, 'templates')),
+    os.path.normpath(os.path.join(BASE_DIR, 'templates')),
 )
 
 INSTALLED_APPS = (
@@ -165,4 +164,9 @@ LOGGING = {
 # GeoIP configuration
 # This includes GeoLite data created by MaxMind,
 # available from http://www.maxmind.com
-GEOIP_PATH = os.path.normpath(os.path.join(SITE_ROOT, 'extras/geoip'))
+GEOIP_PATH = os.path.normpath(os.path.join(BASE_DIR, 'extras/geoip'))
+
+# Translation files for internationalization and localization.
+LOCALE_PATHS = (
+    os.path.join(BASE_DIR, 'locale'),
+)
