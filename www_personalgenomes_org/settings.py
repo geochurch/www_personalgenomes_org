2dd170e5b8d29a5cc5948bc53dd87053bbb4608f
Madeleine Ball <mpball@gmail.com>
2014-05-13 16:58:11 -0400

    Change DEBUG to False

--- www_personalgenomes_org/settings.py
+++ www_personalgenomes_org/settings.py
@@ -5,7 +5,7 @@ from django.utils.translation import ugettext_lazy as _
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-DEBUG = True
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
@@ -23,7 +23,8 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.personalgenomes.org',
+                 'personalgenomesorg-production.elasticbeanstalk.com']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
